2a4ce4a7d487f7f6fe77930e12874543b8de4b26
MovingToWeb <soleil.fils.88@gmail.com>
2019-11-10 16:05:29 +0530

    initial commit

---
+++ src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techprimers.githubactionsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techprimers/githubactionsexample/GithubActionsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.githubactionsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
